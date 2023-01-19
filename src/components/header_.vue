472894b4a48bc3dd2f8d6a0f7dfc733af60db1ff
raidas <mrraidas@gmail.com>
2023-01-19 16:58:47 +0200

    update network button

--- src/components/header_.vue
+++ src/components/header_.vue
@@ -4,6 +4,7 @@ import { useThemeVars } from 'naive-ui'
 
 const themeVars = useThemeVars()
 const emits = defineEmits(['logoClick'])
+const selectedNetwork = 'Mainnet'
 
 </script>
 
@@ -28,14 +29,16 @@ const emits = defineEmits(['logoClick'])
         </n-element>
       </template>
       <template #extra>
-        <n-dropdown trigger="hover" :options="[{label: 'mainnet'}, {label: 'berkeley'}]" @select="">
+        <n-dropdown trigger="hover" :options="[{label: 'Mainnet'}, {label: 'Berkeley', disabled: true}]" @select="">
           <n-button tertiary type="info" size="large">
             <template #icon>
               <n-icon :color="themeVars.primaryColor">
                 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 24 24"><path fill-opacity=".3" d="M23.64 7c-.45-.34-4.93-4-11.64-4C5.28 3 .81 6.66.36 7L12 21.5L23.64 7z" fill="currentColor"></path><path d="M3.53 10.95L12 21.5l8.47-10.55C20.04 10.62 16.81 8 12 8s-8.04 2.62-8.47 2.95z" fill="currentColor"></path></svg>
               </n-icon>
             </template>
-            Network
+            <template #default>
+              <n-text type="primary"> {{ selectedNetwork }} </n-text>
+            </template>
           </n-button>
         </n-dropdown>
       </template>
