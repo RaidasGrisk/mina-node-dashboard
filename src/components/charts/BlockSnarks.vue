bcfdf7b774715a12dc6fa6129c32ac3ef861bf25
raidas <mrraidas@gmail.com>
2023-01-20 15:43:14 +0200

    init

---
+++ src/components/charts/BlockSnarks.vue
@@ -0,0 +1,126 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import StatsCard from '../../components/StatsCard.vue'
+import { useThemeVars } from 'naive-ui'
+
+import { LineChart } from 'vue-chart-3';
+import { Chart, registerables } from "chart.js";
+
+Chart.register(...registerables);
+
+const themeVars = useThemeVars()
+
+const data = ref({})
+const options = {
+  plugins: {
+    legend: {display: false}
+  },
+  scales: {
+    x: {
+      display: false,
+      border: {
+        display: false
+      },
+      grid: {
+        display: false,
+        drawOnChartArea: false,
+        drawTicks: false,
+      }
+    },
+    y: {
+      display: false,
+      border: {
+        display: false
+      },
+      grid: {
+        display: false,
+        drawOnChartArea: false,
+        drawTicks: false,
+      }
+    }
+  }
+}
+
+const chartProps = {
+  chartName: 'Snarks per block',
+  additionalValues: [
+    {value: null, text: 'snarks'},
+    {value: null, text: 'avg. over the last 100 blocks', precision: 0}
+  ],
+  mainValue: null,
+  changeValue: null,
+  description: `
+  <p>
+    What are snarks?
+  </p>
+  `
+}
+
+const loading = ref(false)
+
+const loadData = async () => {
+
+  loading.value = true
+
+  // config
+  const url = 'https://graphql.minaexplorer.com/'
+
+  // API request
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: `
+      query MyQuery {
+        blocks(query: {canonical: true}, limit: 100, sortBy: BLOCKHEIGHT_DESC) {
+          blockHeight
+          snarkJobs {
+            workIds
+          }
+        }
+      }
+        `
+    }),
+  })
+
+  let response_ = await response.json()
+
+  // reverse
+  response_ = response_.data.blocks.reverse()
+
+  // set data element values
+  data.value = {
+    labels: response_.map(i => i.blockHeight),
+    datasets: [
+      {
+        data: response_.map(i => i.snarkJobs.length),
+        backgroundColor: [themeVars.value.infoColor],
+      },
+    ],
+  }
+
+  // set other values
+  let array_ = response_.map(i => i.snarkJobs.length)
+  chartProps.additionalValues[0].value = array_.slice(-1)[0]
+  chartProps.additionalValues[1].value = array_.reduce((a, b) => a + b, 0) / array_.length
+
+  loading.value = false
+}
+
+onMounted( async () => {
+  loadData()
+})
+
+</script>
+
+<template>
+  <StatsCard :data="chartProps" :loading="loading" @reload="loadData">
+    <LineChart :chartData="data" :width="150" :height="100" :options="options" />
+  </StatsCard>
+</template>
+
+<style scoped>
+
+</style>
