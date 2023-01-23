ce234f0b23ce26e0107c2eedf2e4a98b99caf6b2
raidas <mrraidas@gmail.com>
2023-01-23 13:53:39 +0200

    change grid params and max-width

--- src/App.vue
+++ src/App.vue
@@ -54,10 +54,10 @@ const reload = () => {
       <n-loading-bar-provider>
         <n-layout>
           <header_ @logoClick="reload" />
-          <n-layout-content content-style="max-width: 132vh; margin: 0 auto; padding: 2em;">
+          <n-layout-content content-style="max-width: 140vh; margin: 0 auto; padding: 2em;">
             <GeneralStats ref="generalStatsRef"/>
             <div style="padding: 3em 0em 3em 0em;">
-              <n-grid :x-gap="12" :y-gap="12" cols="1 s:2 m:3 l:4 xl:4 2xl:4" responsive="screen">
+              <n-grid :x-gap="12" :y-gap="12" cols="1 s:2 m:3 l:3 xl:4 2xl:4" responsive="screen">
                 <n-grid-item v-for="chart_ in charts">
                   <component v-bind:is="chart_"></component>
                 </n-grid-item>
