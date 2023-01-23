cbd5d98696972966b943c7c22a1eff317244950a
raidas <mrraidas@gmail.com>
2023-01-23 13:53:08 +0200

    add localstorage / overwrite info color for light and dark

--- src/store/modules/theme.js
+++ src/store/modules/theme.js
@@ -2,13 +2,16 @@ import { darkTheme, lightTheme } from 'naive-ui'
 import { useThemeVars } from 'naive-ui'
 
 const state = () => ({
-  themeIsLight: true,
+  themeIsLight: JSON.parse(localStorage.getItem('themeIsLight')),
   lightTheme: lightTheme,
   darkTheme: darkTheme,
   themeOverrides: {
     LoadingBar: {
       colorLoading: '#2081f0', // cant get ThemeVars to work here, so hardcoding
       height: '10px'
+    },
+    common: {
+      infoColor: '#2080f0' // keep same for light and dark
     }
   }
 })
@@ -33,6 +36,7 @@ const actions = {
 
 const mutations = {
   changeTheme(state) {
+    localStorage.setItem('themeIsLight', !state.themeIsLight)
     state.themeIsLight = !state.themeIsLight
   }
 }
