d54c04278201d48d2235e05873717fdf5ba2ad90
raidas <mrraidas@gmail.com>
2023-02-27 12:49:28 +0200

    add @vercel/analytics

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ import App from './App.vue'
 // naive-ui
 import naive from 'naive-ui'
 
+// vercel analytics
+import { inject } from '@vercel/analytics'
+inject()
+
 const app = createApp(App)
 app.use(naive)
 app.use(store)
