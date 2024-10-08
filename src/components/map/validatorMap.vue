586b9a72eccf6a48935e6f8dc987740ef4fe0b75
raidas <mrraidas@gmail.com>
2024-10-08 15:24:24 +0300

    add marker popups

--- src/components/map/validatorMap.vue
+++ src/components/map/validatorMap.vue
@@ -1,23 +1,26 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import data from './data.json';
+import { ref, onMounted } from 'vue'
+import raw_data from './data.json'
 
-import * as L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-import 'leaflet.markercluster/dist/MarkerCluster.css';
-import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
-import "leaflet.markercluster";
-import "leaflet.heat";
+import * as L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet.markercluster/dist/MarkerCluster.css'
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
+import 'leaflet.markercluster'
+import 'leaflet.heat'
 
-const map = ref(null);
-const mapContainer = ref(null);
+const map = ref(null)
+const mapContainer = ref(null)
 
 const initMap = () => {
 
-  // data prep
-  const coordData = data.map(
-    point => [point.location.latitude, point.location.longitude]
-  )
+  // proprocess data by filtering out some keys
+  const keysToKeep = ['datacenter', 'company', 'abuse', 'asn', 'location']
+  const data = raw_data.map(json => {
+    const json_ = {}
+    keysToKeep.forEach(key => { json_[key] = json[key]})
+    return json_
+  })
 
   // init map
   map.value = L.map(mapContainer.value, {
@@ -31,7 +34,7 @@ const initMap = () => {
   }).addTo(map.value)
 
   // init heat layer
-  L.heatLayer(coordData, { 
+  L.heatLayer(data.map(item => [item.location.latitude, item.location.longitude]), { 
     radius: 25,
     blur: 15,
     maxZoom: 18,
@@ -43,8 +46,12 @@ const initMap = () => {
     showCoverageOnHover: false,
     singleMarkerMode: true,
   })
-  coordData.forEach(coords => {
-    L.marker(coords).addTo(markers)
+  data.forEach(item => {
+    L.marker([item.location.latitude, item.location.longitude])
+    .bindPopup(
+      JSON.stringify(item, null, 2).replace(/\n/g, '<br>').replace(/[ ]/g, '&nbsp;')
+    )
+    .addTo(markers)
   })
   markers.addTo(map.value)
 
@@ -52,7 +59,7 @@ const initMap = () => {
 
 onMounted(() => {
   setTimeout(() => { initMap() }, 100) 
-  console.log(data)
+  console.log(raw_data)
 })
 
 </script>
@@ -62,4 +69,21 @@ onMounted(() => {
 </template>
 
 <style>
+
+/* popup */
+.leaflet-popup-content-wrapper {
+  background-color: #222;
+}
+
+/* popup content */
+.leaflet-popup-content {
+  font-size: 10px;
+  color: #999;
+}
+
+/* area outside the map */
+.leaflet-container {
+  background-color: #b1aeae00;
+}
+
 </style>
